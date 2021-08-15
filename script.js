ed0a5c6bedde449bb9b3b59fc46847f14c2bb29e
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2021-08-15 20:56:32 +0800

    Update script.js
    Added sound

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const yeetbutton = document.querySelector('.counter-section button');
 let counter = document.querySelector('.countNum h2')
 console.log(yeetbutton);
-
+let clickSound = new Audio('MinecraftPlaceSound.mp3');
 if(yeetbutton!=null){
-    yeetbutton.addEventListener('click', countUp);
+    yeetbutton.addEventListener('click', countUp,);
+    
 }
 function countUp(){
     counter.innerHTML++;
+    clickSound = null;
+    clickSound = new Audio('MinecraftPlaceSound.mp3');
+    clickSound.play();
 }
